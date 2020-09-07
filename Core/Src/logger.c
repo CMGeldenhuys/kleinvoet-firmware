b719de1de2694bc4bb052f208b41cac8c8f4f020
CM Geldenhuys <20198329@sun.ac.za>
2020-09-07 14:46:49 +0200

    Add color logging

--- Core/Src/logger.c
+++ Core/Src/logger.c
@@ -12,41 +12,56 @@
 
 extern RTC_HandleTypeDef hrtc;
 
-static const char * LOG_Lvl_str_[] = {
-  "DBUG",
-  "INFO",
-  "WARN",
-  "ERR",
+static const char *LOG_Lvl_str_[] = {
+        "DBUG",
+        "INFO",
+        "WARN",
+        "ERR",
 };
 
-int LOG_timestamp_(const char* funcName, LOG_Lvl_e lvl, char *buf);
+static const char *LOG_color_code_[] = {
+        LOG_COLOR_FG_DEFAULT,
+        LOG_COLOR_FG_BLUE,
+        LOG_COLOR_FG_YELLOW,
+        LOG_COLOR_FG_RED
+};
 
-int LOG_log(const char *funcName, LOG_Lvl_e lvl, char *fmt, ...)
+int LOG_timestamp_ (const char *funcName, LOG_Lvl_e lvl, char *buf);
+
+int LOG_log (const char *funcName, LOG_Lvl_e lvl, char *fmt, ...)
 {
   va_list args;
   va_start(args, fmt);
 
   // Create working buffer
   char workBuf[LOG_MSG_INFO_LEN + LOG_MSG_LEN + sizeof(LOG_EOL)];
-  int infoLen = LOG_timestamp_(funcName, lvl, workBuf);
+  int  infoLen = LOG_timestamp_(funcName, lvl, workBuf);
 
   if (infoLen <= 0) return infoLen;
 
   // Clamp infoLen to max
   if (infoLen > LOG_MSG_INFO_LEN) infoLen = LOG_MSG_INFO_LEN;
 
-  int msgLen = vsnprintf(workBuf+infoLen, LOG_MSG_LEN, fmt, args);
+  int msgLen = vsnprintf(workBuf + infoLen, LOG_MSG_LEN, fmt, args);
 
   if (msgLen <= 0) return msgLen;
 
   // Clamp msgLen to max
   if (msgLen > LOG_MSG_LEN) msgLen = LOG_MSG_LEN;
 
-  strcat(workBuf+msgLen, LOG_EOL);
-  // Remove NULL termination since now byte array
-  size_t len = msgLen + infoLen + sizeof(LOG_EOL) - 1;
+  // Compute message length (faster than using strlen)
+  size_t len = msgLen + infoLen;
+
+  strcat(workBuf + len, LOG_EOL);
+  len += sizeof(LOG_EOL);
+
   // Persist Log entry
-  int bytesWritten = LOG_write((uint8_t *) workBuf, len);
+  // -1 : remove NULL term from string
+  size_t tmp;
+  if((tmp = strlen(workBuf)) != len-1 ){
+    ERR("Fokop %u", tmp);
+  }
+  int bytesWritten = LOG_write((uint8_t *) workBuf, len - 1);
 
   if (bytesWritten > 0 && lvl == LOG_ERR) {
     // On Err force Log cache to be written
@@ -56,28 +71,38 @@ int LOG_log(const char *funcName, LOG_Lvl_e lvl, char *fmt, ...)
   return bytesWritten;
 }
 
-int LOG_timestamp_(const char* funcName, LOG_Lvl_e lvl, char *buf)
+int LOG_timestamp_ (const char *funcName, LOG_Lvl_e lvl, char *buf)
 {
   RTC_TimeTypeDef sTime = {0};
   RTC_DateTypeDef sDate = {0};
 
   HAL_RTC_GetTime(&hrtc, &sTime, RTC_FORMAT_BCD);
   HAL_RTC_GetDate(&hrtc, &sDate, RTC_FORMAT_BCD);
-
+#ifdef LOG_COLOR
+  return snprintf(buf, LOG_MSG_INFO_LEN,
+                  "[%s%4s:%-16.16s] %02x-%02x-%02x %02x:%02x:%02x.%-3lu %s- ",
+                  LOG_color_code_[lvl],
+                  LOG_Lvl_str_[lvl], funcName,
+                  sDate.Year, sDate.Month, sDate.Date,
+                  sTime.Hours, sTime.Minutes, sTime.Seconds, sTime.SubSeconds,
+                  LOG_COLOR_RESET
+  );
+#else
   return snprintf(buf, LOG_MSG_INFO_LEN,
                      "[%4s:%-16.16s] %02x-%02x-%02x %02x:%02x:%02x.%-3lu - ",
                      LOG_Lvl_str_[lvl], funcName,
                      sDate.Year, sDate.Month, sDate.Date,
                      sTime.Hours, sTime.Minutes, sTime.Seconds, sTime.SubSeconds
                  );
+#endif
 }
 
-__WEAK int LOG_write(__unused uint8_t *buf, __unused size_t len)
+__WEAK int LOG_write (__unused uint8_t *buf, __unused size_t len)
 {
   return -1;
 }
 
-__WEAK int LOG_flush()
+__WEAK int LOG_flush ()
 {
   return -1;
 }
