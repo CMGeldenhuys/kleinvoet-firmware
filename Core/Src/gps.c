c5fd6c707b1ef3db7947efc46404f29c6f2c6a5c
CM Geldenhuys <20198329@sun.ac.za>
2020-09-15 17:43:08 +0200

    Add str quotes to column headings

--- Core/Src/gps.c
+++ Core/Src/gps.c
@@ -55,7 +55,7 @@ int GPS_init (UART_HandleTypeDef *uart)
           FA_CREATE_ALWAYS | FA_WRITE) <= 0)
     return -2;
   DBUG("Writing header to file");
-  f_printf(gps.fp, "Sample,Date Time,GPS Time of Week,Time Accuracy" FATFS_EOL);
+  f_printf(gps.fp, "\"Sample\",\"Date/Time\",\"GPS Time of Week\",\"Time Accuracy\"" FATFS_EOL);
   if (GPS_configureUBX_() <= 0) return -3;
   return 1;
 }
@@ -308,7 +308,7 @@ int GPS_processCmdNav_ (const GPS_UBX_cmd_t *cmd)
         }
         // Best attempt timestamping
         DBUG("Timestamping");
-        f_printf(gps.fp, "%lu,%4u-%02u-%02u %02u:%02u:%02u.%lu,%lu,%lu",
+        f_printf(gps.fp, "%lu,%4u-%02u-%02u %02u:%02u:%02u.%lu,%lu,%lu" FATFS_EOL,
                  gps.adcTimestamp,
                  cmd_t->year, cmd_t->month, cmd_t->day,
                  cmd_t->hour, cmd_t->min, cmd_t->sec, cmd_t->nano,
