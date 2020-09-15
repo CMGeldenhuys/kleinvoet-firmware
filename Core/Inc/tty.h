06cd35c414fc542aaa0596db264a6ff6b822b611
CM Geldenhuys <20198329@sun.ac.za>
2020-09-15 10:27:38 +0200

    Add args for TTY commands

--- Core/Inc/tty.h
+++ Core/Inc/tty.h
@@ -16,60 +16,83 @@ extern "C" {
 #include "serial.h"
 
 #ifndef TTY_SERVICE_INTERVAL
-  #define TTY_SERVICE_INTERVAL 1000
+#define TTY_SERVICE_INTERVAL 1000
 #endif
 
 #ifndef TTY_GREETING
-  #define TTY_GREETING "Hello, World!"
+#define TTY_GREETING "Hello, World!"
 #endif
 
 #ifndef TTY_CMD_BUF
-  #define TTY_CMD_BUF 128
+#define TTY_CMD_BUF 128
 #endif
 
 #ifndef TTY_CMD_LST
-  #define TTY_CMD_LST 8
+#define TTY_CMD_LST 16
 #endif
 
 #ifndef TTY_SCREEN_WIDTH
-  #define TTY_SCREEN_WIDTH 80
+#define TTY_SCREEN_WIDTH 80
+#endif
+
+#ifndef TTY_ARGS_SPLIT_TOKEN
+#define TTY_ARGS_SPLIT_TOKEN " "
+#endif
+
+#ifndef TTY_ARGS_LEN
+#define TTY_ARGS_LEN 4
+#endif
+
+#ifndef TTY_EOL
+#define TTY_EOL SERIAL_EOL
 #endif
 
 typedef struct {
-	const char* command;
-	int (*func)(int argc, char *argv[]);
-}TTY_Command_t;
+    const char *name;
+
+    int (*func) (int argc, char *argv[]);
+} TTY_Command_t;
 
 typedef struct {
-	char * PS;
-	uint32_t tick;
-	struct {
-		uint8_t buffer[TTY_CMD_BUF];
-		uint8_t * pos;
-		size_t len;
-	}command;
+    char *PS;
+    uint32_t tick;
+    struct {
+        uint8_t buffer[TTY_CMD_BUF];
+        uint8_t *pos;
+        size_t len;
+    }        command;
 
-	TTY_Command_t commandList[TTY_CMD_LST];
+    TTY_Command_t commandList[TTY_CMD_LST];
 
-	Serial_t* serial;
-}TTY_t;
+    Serial_t *serial;
+} TTY_t;
 
 extern TTY_t tty;
 
-int TTY_init(UART_HandleTypeDef* uart);
-int TTY_yield();
-int TTY_registerCommand(const char* command, int (*func)(int argc, char *argv[]));
-int TTY_println(const char *str);
-int TTY_print(const char *str);
-int TTY_write(uint8_t *buf, size_t len);
-int TTY_available();
-uint8_t TTY_read();
-int TTY_resetDevice(int argc, char *argv[]);
+int TTY_init (UART_HandleTypeDef *uart);
+
+int TTY_yield ();
+
+int TTY_registerCommand (const char *command, int (*func) (int argc, char *argv[]));
+
+int TTY_println (const char *str);
+
+int TTY_print (const char *str);
+
+int TTY_write (uint8_t *buf, size_t len);
+
+int TTY_available ();
+
+uint8_t TTY_read ();
+
+int TTY_resetDevice (int argc, char *argv[]);
 
 // TODO: Find fix for linker to pick up
 #define TTY_PRINTF
 #ifdef TTY_PRINTF
-int TTY_printf(const char* fmt, ...);
+
+int TTY_printf (const char *fmt, ...);
+
 #endif
 
 #ifdef __cplusplus
--- Core/Src/tty.c
+++ Core/Src/tty.c
@@ -10,22 +10,33 @@
 #include <string.h>
 
 #ifdef TTY_PRINTF
+
 #include <stdio.h>
 #include <stdarg.h>
+
 char workBuf_[TTY_SCREEN_WIDTH];
 #endif
 
-int TTY_greet_();
-void TTY_clearCommandBuffer_();
-int TTY_processCommand_();
-int TTY_PS_();
+int TTY_greet_ ();
+
+void TTY_clearCommandBuffer_ ();
+
+int TTY_processCommand_ ();
+
+int TTY_PS_ ();
+
+void TTY_splitArgs_ (char *cmdBuf, char **cmd, int *argc, char *args[]);
+
+#ifdef DEBUG
+int TTY_testArgs(int argc, char *args[]);
+#endif
 
 TTY_t tty = {0};
 
-int TTY_init(UART_HandleTypeDef* uart)
+int TTY_init (UART_HandleTypeDef *uart)
 {
-	tty.serial = (Serial_t*) malloc(sizeof(Serial_t));
-	Serial_wrap(tty.serial, uart);
+  tty.serial = (Serial_t *) malloc(sizeof(Serial_t));
+  Serial_wrap(tty.serial, uart);
   TTY_clearCommandBuffer_();
 
 #ifdef TTY_GREETING
@@ -36,165 +47,196 @@ int TTY_init(UART_HandleTypeDef* uart)
   // Register TTY default commands
   TTY_registerCommand("reset", &TTY_resetDevice);
 
-	if(!*tty.PS) tty.PS = SERIAL_EOL "> ";
-  TTY_PS_();
-	return 1;
-}
-
-void TTY_deint()
-{
-	free(tty.serial);
-}
-
-int TTY_greet_()
-{
-	return Serial_println(tty.serial, TTY_GREETING);
-}
-
-int TTY_yield()
-{
-	if( Serial_available(tty.serial) > 0 ) {
-		char recv = (char) Serial_read(tty.serial);
-		// Check if printable character
-		// ' ' <= recv <= '~'
-		if(32u <= recv && recv <= 126u){
-			// Store rx char
-			tty.command.len++;
-			*(tty.command.pos ++) = recv;
-
-			// Echo back
-#ifndef TTY_ECHO_DISABLE
-			Serial_write(tty.serial, (uint8_t*) &recv, 1);
-#endif
-		}
-		// Detect Backspace
-		else if(recv == 127u && tty.command.len > 0){
-			tty.command.len--;
-			tty.command.pos--;
-
-#ifndef TTY_ECHO_DISABLE
-			// Send Backspace
-			Serial_print(tty.serial, SERIAL_BS);
-#endif
-		}
-		// Check for return key
-		// End of command
-		else if(recv == '\r') {
 #ifdef DEBUG
-			Serial_println(tty.serial, "");
-			Serial_print(tty.serial, "< ");
-			Serial_write(tty.serial, tty.command.buffer, tty.command.len);
-			Serial_println(tty.serial, "");
+  TTY_registerCommand("args", &TTY_testArgs);
 #endif
-			// Terminate command string
-			(*tty.command.pos) = '\0';
+
+  if (!*tty.PS) tty.PS = SERIAL_EOL "> ";
+  TTY_PS_();
+  return 1;
+}
+
+void TTY_deint ()
+{
+  free(tty.serial);
+}
+
+int TTY_greet_ ()
+{
+  return Serial_println(tty.serial, TTY_GREETING);
+}
+
+int TTY_yield ()
+{
+  if (Serial_available(tty.serial) > 0) {
+    char recv = (char) Serial_read(tty.serial);
+    // Check if printable character
+    // ' ' <= recv <= '~'
+    if (32u <= recv && recv <= 126u) {
+      // Store rx char
+      tty.command.len++;
+      *(tty.command.pos++) = recv;
+
+      // Echo back
+#ifndef TTY_ECHO_DISABLE
+      Serial_write(tty.serial, (uint8_t *) &recv, 1);
+#endif
+    }
+      // Detect Backspace
+    else if (recv == 127u && tty.command.len > 0) {
+      tty.command.len--;
+      tty.command.pos--;
+
+#ifndef TTY_ECHO_DISABLE
+      // Send Backspace
+      Serial_print(tty.serial, SERIAL_BS);
+#endif
+    }
+      // Check for return key
+      // End of command
+    else if (recv == '\r') {
+#ifdef DEBUG
+      Serial_println(tty.serial, "");
+      Serial_print(tty.serial, "< ");
+      Serial_write(tty.serial, tty.command.buffer, tty.command.len);
+      Serial_println(tty.serial, "");
+#endif
+      // Terminate command string
+      (*tty.command.pos) = '\0';
 
       TTY_processCommand_();
       TTY_clearCommandBuffer_();
       TTY_PS_();
-		}
-	}
-	return 1;
+    }
+  }
+  return 1;
 }
 
 //TODO: Make inline
-int TTY_println(const char *str)
+int TTY_println (const char *str)
 {
   return Serial_println(tty.serial, str);
 }
 
-int TTY_print(const char *str)
+int TTY_print (const char *str)
 {
   return Serial_print(tty.serial, str);
 }
 
-int TTY_write(uint8_t *buf, size_t len)
+int TTY_write (uint8_t *buf, size_t len)
 {
   return Serial_write(tty.serial, buf, len);
 }
 
-int TTY_available()
+int TTY_available ()
 {
   return Serial_available(tty.serial);
 }
 
-uint8_t TTY_read()
+uint8_t TTY_read ()
 {
   return Serial_read(tty.serial);
 }
 
-int TTY_registerCommand(const char *command, int (*func)(__unused int argc, __unused char *argv[]))
+int TTY_registerCommand (const char *command, int (*func) (__unused int argc, __unused char *argv[]))
 {
-	static size_t idx = 0;
-	// Run out of command space
-	if(idx >= TTY_CMD_LST) return 0;
+  static size_t idx = 0;
+  // Run out of command space
+  if (idx >= TTY_CMD_LST) return 0;
 
-	tty.commandList[idx++] = (TTY_Command_t){
-			.command = command,
-			.func = func
-	};
-	return idx;
+  tty.commandList[idx++] = (TTY_Command_t) {
+          .name = command,
+          .func = func
+  };
+  return idx;
 }
 
 #ifdef TTY_PRINTF
-int TTY_printf(const char* fmt, ...)
+
+int TTY_printf (const char *fmt, ...)
 {
   va_list args;
   va_start(args, fmt);
 
   int len = vsnprintf(workBuf_, TTY_SCREEN_WIDTH, fmt, args);
   // Clamp length
-  TTY_write((uint8_t*)workBuf_,
-            len > TTY_SCREEN_WIDTH ?  TTY_SCREEN_WIDTH : len);
+  TTY_write((uint8_t *) workBuf_,
+            len > TTY_SCREEN_WIDTH ? TTY_SCREEN_WIDTH : len);
   return len > TTY_SCREEN_WIDTH ? -1 * len : len;
 }
+
 #endif
 
-int TTY_processCommand_()
+int TTY_processCommand_ ()
 {
-	//TODO: Split command string into command + args
-	for(size_t idx = 0; idx < TTY_CMD_LST; idx++){
-		// Lookup command
-		TTY_Command_t *cmd = &tty.commandList[idx];
+  //TODO: Split command string into command + args
+  char * cmdStr;
+  char *args[TTY_ARGS_LEN];
+  int argc;
 
-		// Match command string
-		if(strcmp((char*) tty.command.buffer, cmd->command) == 0){
-		  // TODO: send args
-		  DBUG("Command received '%s'", cmd->command);
-			return (*(cmd->func))(0, NULL);
-		}
-	}
-	// Failed to find command
-	// TODO: Report
-	return 0;
+  TTY_splitArgs_((char *) tty.command.buffer, &cmdStr, &argc, args);
+
+  for (size_t idx = 0; idx < TTY_CMD_LST; idx++) {
+    // Lookup command
+    TTY_Command_t *cmd = &tty.commandList[idx];
+
+    // Match command string
+    if (strcmp(cmdStr, cmd->name) == 0) {
+      DBUG("Command received '%s'", cmd->name);
+      return (*(cmd->func))(argc, args);
+    }
+  }
+  TTY_printf("No such command '%s' found", cmdStr);
+  return 0;
 }
 
-void TTY_clearCommandBuffer_()
+void TTY_splitArgs_ (char *cmdBuf, char **cmd, int *argc, char *args[])
 {
-	//TODO: Move buffer to heap (malloc) so can process multiple commands without risk of overwrite
-	tty.command.len = 0;
-	tty.command.pos = tty.command.buffer;
+  *cmd = strtok(cmdBuf, TTY_ARGS_SPLIT_TOKEN);
+  *argc = 0;
+  char * ptr;
+  while ((ptr = strtok(NULL, TTY_ARGS_SPLIT_TOKEN))
+         && *argc < TTY_ARGS_LEN) {
+    DBUG("arg [%d] -> %s", *argc, ptr);
+    args[(*argc)++] = ptr;
+  }
 }
 
-int TTY_PS_()
+void TTY_clearCommandBuffer_ ()
 {
-	return Serial_print(tty.serial, tty.PS);
+  //TODO: Move buffer to heap (malloc) so can process multiple commands without risk of overwrite
+  tty.command.len = 0;
+  tty.command.pos = tty.command.buffer;
 }
 
-int TTY_resetDevice(__unused int argc, __unused char *argv[])
+int TTY_PS_ ()
+{
+  return Serial_print(tty.serial, tty.PS);
+}
+
+int TTY_resetDevice (__unused int argc, __unused char *argv[])
 {
   TTY_println("System is being reset!");
   HAL_NVIC_SystemReset();
   return 1;
 }
 
+int TTY_testArgs(int argc, char *args[])
+{
+  TTY_println("ARGS: ");
+  for(int i = 0; i < argc; i++) {
+    TTY_printf("  [%i] -> '%s'" TTY_EOL,i, args[i]);
+  }
+}
+
+
 #ifdef DEBUG
-int LOG_write(uint8_t *buf, size_t len)
+int LOG_write (uint8_t *buf, size_t len)
 {
   return Serial_write(tty.serial, buf, len);
 }
 
-int LOG_flush()
+int LOG_flush ()
 {
   // TODO: Fix once buffered writer is implemented
   return 1;
