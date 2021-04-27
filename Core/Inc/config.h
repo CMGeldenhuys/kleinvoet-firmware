49ee76f82eb886d2914d096cd64c60c4588a673e
CM Geldenhuys <20198329@sun.ac.za>
2021-04-27 09:39:52 +0200

    Bump version number to alpha

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -14,9 +14,9 @@ extern "C" {
 #define AUTHORS "CM Geldenhuys"
 
 #define VERSION_MAJOR "0"
-#define VERSION_MINOR "4"
-#define VERSION_PATCH "1"
-#define VERSION_RC ""
+#define VERSION_MINOR "5"
+#define VERSION_PATCH "0"
+#define VERSION_RC "alpha"
 
 #define VERSION VERSION_MAJOR "." VERSION_MINOR "." VERSION_PATCH "-" VERSION_RC
 
