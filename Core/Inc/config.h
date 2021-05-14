bc45c5dd055636f2bbe7d232f838d6541a409a59
CM Geldenhuys <20198329@sun.ac.za>
2021-05-14 08:55:49 +0200

    Bump version

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -14,8 +14,8 @@ extern "C" {
 #define AUTHORS "CM Geldenhuys"
 
 #define VERSION_MAJOR "0"
-#define VERSION_MINOR "5"
-#define VERSION_PATCH "2"
+#define VERSION_MINOR "6"
+#define VERSION_PATCH "0"
 #define VERSION_RC "alpha"
 
 #if defined(VERSION_RC)
